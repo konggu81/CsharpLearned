8ac969a0457928703e328167f1cf79b0faa7aa62
konggu81 <yj81_10@msn.com>
2017-06-10 18:09:17 +0800

    泛型弱事件
    第二次提交

---
+++ MyTest01/MyStringTest/MyStringTest/MyStringTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStringTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			StringBuilder greetingBuilder = new StringBuilder("Hello from all the guys at wrox press.", 150);
+			greetingBuilder.AppendFormat("
+		}
+	}
+}
--- MyTest01/myEventTest/myEventTest/Program.cs
+++ MyTest01/myEventTest/myEventTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace myEventTest
 {
@@ -14,19 +15,23 @@ namespace myEventTest
 
             var michael = new Consumer("老王");
             //dealer.NewCarInfo += michael.NewCarIsHere;
-            WeakCarInfoEventManager.AddListener(dealer, michael);
+           // WeakCarInfoEventManager.AddListener(dealer, michael);
+			WeakEventManager<CarDealer, CarInfoEventArgs>.AddHandler(dealer, "NewCarInfo", michael.NewCarIsHere);
+
+
 
             dealer.NewCar("法拉利");
 
             var sebastian = new Consumer("老张");
             //dealer.NewCarInfo += sebastian.NewCarIsHere;
-            WeakCarInfoEventManager.AddListener(dealer, sebastian);
+            //WeakCarInfoEventManager.AddListener(dealer, sebastian);
+			WeakEventManager<CarDealer, CarInfoEventArgs>.AddHandler(dealer, "NewCarInfo", sebastian.NewCarIsHere);
 
 
             dealer.NewCar("奔驰");
             //dealer.NewCarInfo -= michael.NewCarIsHere;
-            WeakCarInfoEventManager.RemoveListener(dealer, michael);
-
+            //WeakCarInfoEventManager.RemoveListener(dealer, michael);
+			WeakEventManager<CarDealer, CarInfoEventArgs>.RemoveHandler(dealer, "NewCarInfo", michael.NewCarIsHere);
             dealer.NewCar("奥迪");
 
             Console.ReadKey();

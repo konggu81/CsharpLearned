4c70b0241dd22c5091e823968d5174d722b390fc
konggu81 <yj81_10@msn.com>
2017-06-10 12:34:20 +0800

    委托测试
    委托测试

---
+++ MyTest01/myEventTest/myEventTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myEventTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dealer = new CarDealer();
+
+            var michael = new Consumer("老王");
+            //dealer.NewCarInfo += michael.NewCarIsHere;
+            WeakCarInfoEventManager.AddListener(dealer, michael);
+
+            dealer.NewCar("法拉利");
+
+            var sebastian = new Consumer("老张");
+            //dealer.NewCarInfo += sebastian.NewCarIsHere;
+            WeakCarInfoEventManager.AddListener(dealer, sebastian);
+
+
+            dealer.NewCar("奔驰");
+            //dealer.NewCarInfo -= michael.NewCarIsHere;
+            WeakCarInfoEventManager.RemoveListener(dealer, michael);
+
+            dealer.NewCar("奥迪");
+
+            Console.ReadKey();
+        }
+    }
+}

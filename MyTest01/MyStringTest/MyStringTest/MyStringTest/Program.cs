8321681c9b3247313989811f27c5c8241af542bc
yangjin <jin.yang81@foxmail.com>
2017-06-11 16:27:38 +0800

    字符串操作
    补全提交

--- MyTest01/MyStringTest/MyStringTest/MyStringTest/Program.cs
+++ MyTest01/MyStringTest/MyStringTest/MyStringTest/Program.cs
@@ -8,10 +8,31 @@ namespace MyStringTest
 {
 	class Program
 	{
-		static void Main(string[] args)
-		{
-			StringBuilder greetingBuilder = new StringBuilder("Hello from all the guys at wrox press.", 150);
-			greetingBuilder.AppendFormat("
-		}
+        static void Main(string[] args)
+        {
+            StringBuilder greetingBuilder = new StringBuilder("Hello from all the guys at wrox press.", 150);
+            greetingBuilder.AppendFormat("We do hope you enjoy this book as much as we " + "enjoyed writing it");
+
+            Console.WriteLine("Not Encode:\n" + greetingBuilder);
+
+            for (int i = 'z'; i > 'a'; i--)
+            {
+                char old1 = (char)i;
+                char new1 = (char)(i + 1);
+
+                greetingBuilder = greetingBuilder.Replace(old1, new1);
+            }
+
+            for (int i = 'Z'; i > 'A'; i--)
+            {
+                char old1 = (char)i;
+                char new1 = (char)(i + 1);
+
+                greetingBuilder = greetingBuilder.Replace(old1, new1);
+            }
+
+            Console.WriteLine("Encoded:\n" + greetingBuilder);
+            Console.ReadKey();
+        }
 	}
 }
